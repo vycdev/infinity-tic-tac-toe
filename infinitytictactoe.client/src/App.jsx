21baf355f5b7f44406ed509aabec14f168f8d2bc
Victor Sandu <vyctor661@gmail.com>
2025-03-10 21:40:08 +0200

    font and scoreboard

--- infinitytictactoe.client/src/App.jsx
+++ infinitytictactoe.client/src/App.jsx
@@ -2,14 +2,20 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+    const [pointsX, setPointsX] = useState(0);
+    const [pointsO, setPointsO] = useState(0);
 
     useEffect(() => {
 
     }, []);
 
     return (
-        <div id="content">
+        <div id="content" className="kode-mono">
             <h1 className="fade-away">Infinity Tic Tac Toe</h1>
+            <div className="scoreBoard">
+                <div>Player X: {pointsX}</div>
+                <div>Player O: {pointsO}</div>
+            </div>
             <canvas className="canvas"></canvas>
         </div>
     );
